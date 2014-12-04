0e65918081db285c2328bf699e31112376e6c0c3
elkabumi <elkabuminew@gmail.com>
2014-12-04 13:53:14 +0700

    04-12-2014

--- lib/config.php
+++ lib/config.php
@@ -2,6 +2,6 @@
 ob_start();
 session_start();
 $con = mysql_connect("localhost","root","");
-mysql_select_db("award", $con);
+mysql_select_db("farmer_management", $con);
 unset($_SESSION['menu_active']);
 ?>
